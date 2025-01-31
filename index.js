37c7a526ad2fe680b8d4f3a75231ffbb11d585d9
Yash Mulik <yashmulik95@gmail.com>
2025-01-31 21:52:50 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 app.use(bodyParser.json());
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
+  .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
 app.use(authRoutes);

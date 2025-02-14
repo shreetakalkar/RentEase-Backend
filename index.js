16850fb94eeacdc1f9f5eff90a88e0a4f526c17a
Yash Mulik <yashmulik95@gmail.com>
2025-02-14 23:01:09 +0530

    cors added

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(express.json());
+app.use(
+  cors({
+    // origin: "http://localhost:5173",
+    // origin: "https://mastercode.netlify.app",
+    origin: "*",
+    methods: ["POST", "GET"],
+    credentials: true,
+    optionSuccessStatus: 200,
+  })
+);
 
 mongoose
   .connect(process.env.MONGO_URI)
